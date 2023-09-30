8f1db9cbbd738c7ea23f4fa1658f37d74db2a18e
Valeriy Lvov <tepliy_shov@mail.ru>
2023-09-30 10:14:50 +0500

    create add_commendation func

--- datacenter/func.py
+++ datacenter/func.py
@@ -1,7 +1,11 @@
-from datacenter.models import Chastisement
+from datacenter.models import Chastisement, Schoolkid, Subject, Teacher, Commendation
 
 
 def remove_chastisements(schoolkid):
     zam = Chastisement.objects.filter(schoolkid__full_name__contains=f'{schoolkid}')
     for z in zam:
         z.delete()
+
+
+def add_commendation(text, created, schoolkid, subject, teacher):
+    Commendation.objects.create(text=f'{text}', created=f'{created}', schoolkid=Schoolkid.objects.filter(full_name=f'{schoolkid}')[0], subject=Subject.objects.filter(title=f'{subject}')[0], teacher=Teacher.objects.filter(full_name=f'{teacher}')[0])
